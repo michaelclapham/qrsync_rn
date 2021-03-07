08076f5a94c37d85bb77bf9b1793b5bf32cbec48
Michael Clapham <michaeljclapham@gmail.com>
2021-03-07 19:48:01 +0000

    Added QR code scanner

--- AboutScreen.tsx
+++ AboutScreen.tsx
@@ -1,12 +1,54 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { BarCodeReadEvent, RNCamera } from "react-native-camera";
+import QRCodeScanner from "react-native-qrcode-scanner";
 
 const AboutScreen: React.FC = () => {
+
+    const onSuccess = (event: BarCodeReadEvent) => {
+        console.log("Success?");
+        Alert.alert("QR Code Scanned", event.data);
+    }
+
     return (
         <View>
             <Text>About</Text>
+            <QRCodeScanner
+                onRead={onSuccess}
+                topContent={
+                    <Text style={styles.centerText}>
+                        Go to{' '} <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+                        your computer and scan the QR code.
+                     </Text>
+                }
+                bottomContent={
+                    <TouchableOpacity style={styles.buttonTouchable}>
+                        <Text style={styles.buttonText}>OK. Got it!</Text>
+                    </TouchableOpacity>
+                }
+            />
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    centerText: {
+      flex: 1,
+      fontSize: 18,
+      padding: 32,
+      color: '#777'
+    },
+    textBold: {
+      fontWeight: '500',
+      color: '#000'
+    },
+    buttonText: {
+      fontSize: 21,
+      color: 'rgb(0,122,255)'
+    },
+    buttonTouchable: {
+      padding: 16
+    }
+  });
 
 export default AboutScreen;
